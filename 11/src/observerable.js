b056f97a51eded5233ac7646762e3b7277b3ffd9
Toon Ketels <toon@puresign.be>
2012-08-04 09:04:07 +0200

    Refactoring observerable to remove the init function.

--- 11/src/observerable.js
+++ 11/src/observerable.js
@@ -2,20 +2,34 @@ tddjs.namespace("util");
 
 (function () {
 	function Observerable() {
-		this.observers = [];
+
 	}
 
 	function addObserver(observer) {
+		if(!this.observers) {
+			this.observers = [];
+		}
+		if(typeof observer !== 'function') {
+			throw new TypeError("This is not a function.");
+		}
 		this.observers.push(observer);
 	}
 
 	function hasObserver(observer) {
+		if(!this.observers) {
+			return false;
+		}
 		return (this.observers.indexOf(observer) !== -1);
 	}
 
 	function notifyObservers() {
+		if(!this.observers) {
+			return;
+		}
 		for (var i = 0, l = this.observers.length; i < l; i++) {
-			this.observers[i].apply(this, arguments);
+			try {
+				this.observers[i].apply(this, arguments);
+			} catch (e) {}
 		}
 	}
 
--- 11/test/observerable-test.js
+++ 11/test/observerable-test.js
@@ -40,4 +40,45 @@ TestCase("ObserverableAddObserverTest", {
   		assertEquals(["String", 1, 32], actual);
     }
 
+  , "test should throw for uncallable observer": function () {
+  		var observerable = new tddjs.util.Observerable();
+
+  		assertException(function () {
+  			observerable.addObserver({});
+  		}, "TypeError");
+    }
+
+  , "test should notify all even when some fail": function () {
+  		var observerable = new tddjs.util.Observerable();
+  		var observer1 = function () { throw new Error('Catch me if you can.'); };
+  		var observer2 = function () { observer2.called = true; };
+
+  		observerable.addObserver(observer1);
+  		observerable.addObserver(observer2);
+  		observerable.notifyObservers();
+
+  		assertTrue(observer2.called);
+    }
+
+  , "test should call observers in the order they were added": function () {
+  		var observerable = new tddjs.util.Observerable();
+  		var calls = [];
+  		var observer1 = function () { calls.push(observer1); };
+  		var observer2 = function () { calls.push(observer2); };
+
+  		observerable.addObserver(observer1);
+  		observerable.addObserver(observer2);
+  		observerable.notifyObservers();
+
+  		assertEquals(observer1, calls[0]);
+  		assertEquals(observer2, calls[1]);
+    }
+
+  , "test should should not fail if no observers": function () {
+  		var observerable = new tddjs.util.Observerable();
+
+  		assertNoException(function () {
+  			observerable.notifyObservers();
+  		});
+    }
 });
